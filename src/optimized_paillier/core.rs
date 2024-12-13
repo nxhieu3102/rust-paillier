29048d89c4ad20a15851774479187982a148e59b
NTTVy03 <trucvy05062003@gmail.com>
2024-12-13 15:17:03 +0700

    update CRT comment

--- src/optimized_paillier/core.rs
+++ src/optimized_paillier/core.rs
@@ -410,17 +410,17 @@ impl<'c, 'm> DecryptCRT<DecryptionKey, RawCiphertext<'c>, RawPlaintext<'m>> for 
         // l(u,n) = (u - 1)/n (mod n)
         // inv_2alpha = (2*alpha)^(-1) mod n
         // n = qp, p and q are primes
-
-        // mp = [l(c^(2*alpha) mod p^2, p) * inv_2alpha] mod p
-        // mq = [l(c^(2*alpha) mod q^2, q) * inv_2alpha] mod q
-
+        
+        // mp = [l(c^(2*alpha) mod p^2, p) * (inv_2alpha mod p)] mod p
+        // mq = [l(c^(2*alpha) mod q^2, q) * (inv_2alpha mod q)] mod q
+        
         let dk_qq = &dk.q * &dk.q; // q^2
         let dk_pp = &dk.p * &dk.p; // p^2
         let dk_pinv = BigInt::mod_inv(&dk.p, &dk.q).unwrap();
         let dk_double_alpha = 2 * &dk.alpha;
         let dk_hp = h(&dk_double_alpha, &dk.p); // (2.alpha)^(-1) mod p
         let dk_hq = h(&dk_double_alpha, &dk.q); // (2.alpha)^(-1) mod q
-                                                // cp = c % p^2; cq = c % q^2
+        // cp = c % p^2; cq = c % q^2
         let (cp, cq) = crt_decompose(c.0.borrow(), &dk_pp, &dk_qq);
         // decrypt in parallel with respectively p and q
         // mp: decrypt, replace all n --> p
@@ -439,6 +439,7 @@ impl<'c, 'm> DecryptCRT<DecryptionKey, RawCiphertext<'c>, RawPlaintext<'m>> for 
                 (&lq * &dk_hq) % &dk.q
             },
         );
+
         // perform CRT
         let m = crt_recombine(mp, mq, &dk.p, &dk.q, &dk_pinv);
         RawPlaintext(Cow::Owned(m))
@@ -448,7 +449,7 @@ impl<'c, 'm> DecryptCRT<DecryptionKey, RawCiphertext<'c>, RawPlaintext<'m>> for 
 // ----------------------- CRT utils
 
 fn h(double_alpha: &BigInt, p: &BigInt) -> BigInt {
-    // compute (2.alpha)^(-1) mod p^2
+    // compute (2.alpha)^(-1) mod p
     BigInt::mod_inv(&double_alpha, p).unwrap()
 }
 
